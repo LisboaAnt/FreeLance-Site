c95c4494ec0c005da7eeb18d25357af7702b2d66
Antonio Lisboa de Carvalho <104227926+LisboaAnt@users.noreply.github.com>
2024-10-08 22:34:30 -0300

    Criando livewere components

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,9 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/outra-rota', fn () => 'oi');
+Route::get('/', [ProjectsController::class, 'index'])-> name('projects.index');
+
+Route::get('project/{project}', [ProjectsController::class, 'show'])->name('projects.show');
